bd8b4a19bd152e59271c30dd5a269662d85e893e
Jhenil Parihar <jhenilparihar@gmail.com>
2023-02-03 21:26:11 +0530

    Intial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { Web3Storage } from "web3.storage";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const client = new Web3Storage({
+  token:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDQ0NmM0OTI0OWRGY0E2MjFlYTY1MjkzYzFhQkIyZWQ2OUZhYTA0MjkiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjA5OTg0OTgyNDgsIm5hbWUiOiJFLUNlcnRpZnkifQ.9tckrWu32QWsyDR2-s8Azgn6suWzjwfwFKNY0Mc3Jm8",
+});
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: "",
+      img: "",
+    };
+  }
+
+  changeHandler = async () => {
+    this.setState({ loading: true });
+    console.log("Hi");
+
+    const fileInput = document.querySelector('input[type="file"]');
+
+    console.log(fileInput);
+
+    // Pack files into a CAR and send to web3.storage
+    const rootCid = await client.put(fileInput.files); // Promise<CIDString>
+
+    console.log(rootCid);
+
+    // Get info on the Filecoin deals that the CID is stored in
+    const info = await client.status(rootCid); // Promise<Status | undefined>
+
+    console.log(info);
+
+    // Fetch and verify files from web3.storage
+    const res = await client.get(rootCid); // Promise<Web3Response | null>
+    console.log(res);
+
+    const files = await res.files(); // Promise<Web3File[]>
+    console.log(files);
+
+    for (const file of files) {
+      console.log(`${file.cid} ${file.name} ${file.size}`);
+      // this.setState({ img: "https://"+file.cid+"/"+file.name});
+      this.retrieve(file.cid);
+    }
+    this.setState({ loading: false});
+  };
+
+  retrieve = async (cid) => {
+    const res = await client.get(cid)
+    console.log(`Got a response! [${res.status}] ${res.statusText}`)
+    console.log(res.url)
+    if (!res.ok) {
+      throw new Error(`failed to get ${cid}`)
+    }
+  
+    // request succeeded! do something with the response object here...
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.img.length > 0 ? (
+          <>
+            <img src={"https://"+this.state.img+".png"} />
+          </>
+        ) : (
+          <>
+            {!this.state.loading ? (
+              <>
+                <h1>Hi Jhenil</h1>
+                <p>hiib</p>
+                <input type="file" name="file" onChange={this.changeHandler} />
+              </>
+            ) : (
+              <>Loading..</>
+            )}
+          </>
+        )}
+      </>
+    );
+  }
 }
 
 export default App;
